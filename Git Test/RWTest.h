0e5ffd1514272cc5895a77d91533ea5103988d63
Ryan Willard <ryanwillard@rwillardphilMM.local>
2014-04-23 22:49:39 -0600

    added test class

---
+++ Git Test/RWTest.h
@@ -0,0 +1,13 @@
+//
+//  RWTest.h
+//  Git Test
+//
+//  Created by Ryan Willard on 4/23/14.
+//  Copyright (c) 2014 Ryan Willard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWTest : NSObject
+
+@end
---
+++ Git Test/RWTest.m
@@ -0,0 +1,13 @@
+//
+//  RWTest.m
+//  Git Test
+//
+//  Created by Ryan Willard on 4/23/14.
+//  Copyright (c) 2014 Ryan Willard. All rights reserved.
+//
+
+#import "RWTest.h"
+
+@implementation RWTest
+
+@end
